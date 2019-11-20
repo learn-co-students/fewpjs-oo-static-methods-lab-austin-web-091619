500d59dd8c5bf4d21bb94f5ab99a3f50015c6fd5
swdecker <swdecker@gmail.com>
2019-11-20 16:57:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+  static titleize(string){
+    let ignore= "the,a,an,but,of,and,for,at,by,from".split(",")
+    return this.capitalize(string).split(" ").map(w=> ignore.includes(w) ? w : w[0].toUpperCase()+w.slice(1) ).join(" ")
+  }
 }
